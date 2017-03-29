88d91864252061d13e6c2a26656f8ca2a16c098c
Anthony Egwu <kaegwu@gmail.com>
2017-03-28 21:35:57 -0500

    initial commit

---
+++ apps/beltapp/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
+    url(r'^register', views.register, name = 'register'),
+    url(r'^logon', views.logon, name = 'logon'),
+    url(r'^dashboard', views.dashboard, name = 'dashboard'),
+    url(r'^addbook$', views.addbook, name = 'addbook'),
+    url(r'^addingbook$', views.addingbook, name = 'addingbook'),
+    url(r'^logout', views.logout, name = 'logout'),
+    url(r'^addreview$', views.addreview, name = 'addreview'),
+    url(r'^bookinfo/(?P<id>\d+)$', views.bookinfo, name = 'bookinfo'),
+    url(r'^userinfo/(?P<id>\d+)$', views.userinfo, name = 'userinfo'),
+]
